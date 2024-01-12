a94279e941e5d25d820a618e851d1e7605dde2aa
Foukhar Sara <118546495+FoukharSara@users.noreply.github.com>
2024-01-13 00:59:18 +0100

    Create add_cat.php

---
+++ add_cat.php
@@ -0,0 +1,11 @@
+<?php
+include("connexion.php");
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $nom=$_POST["categoryName"];
+    $request= "INSERT INTO categorie(LABEL) VALUES('$nom')";
+    $res=mysqli_query($link,$request);
+    header("location: /projetf/dashboard.php");
+    mysqli_close($link);
+}
+
+?>
