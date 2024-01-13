77bd71eda8e8d13e22018a912c4ac7fdc9620ad6
Saddiqe Douaa <126075553+douaa0@users.noreply.github.com>
2024-01-13 14:20:24 +0100

    Update delete_event.php

--- delete_event.php
+++ delete_event.php
@@ -13,18 +13,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['event_delete'])) {
         mysqli_query($link, $delete_comments_query);
     }
 
-    // Delete child records from inscription table
-    $delete_inscription_query = "DELETE FROM inscription WHERE ID_EVENT = $event_delete";
-    mysqli_query($link, $delete_inscription_query);
-
-    // Now, delete the record from the event table
     $delete_event_query = "DELETE FROM event WHERE ID_EVENT = $event_delete";
     if (mysqli_query($link, $delete_event_query)) {
         if (mysqli_affected_rows($link) > 0) {
-            // Event deleted successfully
-            echo "<script>alert('Evenement est supprimée!!'); window.location.href = '/projetf/dashboard.php';</script>";
+            header("Location: dashboard.php");
             exit();
-         
         } else {
             echo "Event not found.";
         }
