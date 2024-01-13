803ab929a68b13aa70d63afa6466af92ba978fe4
Foukhar Sara <118546495+FoukharSara@users.noreply.github.com>
2024-01-13 21:12:28 +0100

    Update detail.php

--- detail.php
+++ detail.php
@@ -2,7 +2,7 @@
 session_start();
 if(isset($_SESSION['loggedin'])&& $_SESSION['loggedin'] == true){
 include('connexion.php');
-session_start();
+
 
 if (isset($_GET['Envoyer'])) {
     $email = $_SESSION['email'];
