a9eb1f25a03d37c6677368846b5153e8ce7f20f9
Saddiqe Douaa <126075553+douaa0@users.noreply.github.com>
2024-01-13 14:26:52 +0100

    Update add_cat.php

--- add_cat.php
+++ add_cat.php
@@ -4,7 +4,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     $nom=$_POST["categoryName"];
     $request= "INSERT INTO categorie(LABEL) VALUES('$nom')";
     $res=mysqli_query($link,$request);
-    header("location: /projetf/dashboard.php");
+    header("location: dashboard.php");
     mysqli_close($link);
 }
 
