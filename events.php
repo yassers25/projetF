668f5a56193165c5d20f9a10b589683503c4ca2e
rihabsem <131304614+rihabsem@users.noreply.github.com>
2024-01-13 13:29:51 +0100

    Update events.php

--- events.php
+++ events.php
@@ -13,7 +13,7 @@ include("connexion.php");
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="event_styles.css">
+    <link rel="stylesheet" href="event_styl.css">
     <title>Events</title>
     <style>
     .icon-github {
